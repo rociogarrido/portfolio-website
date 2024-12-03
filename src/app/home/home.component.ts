540b3ad449aeca48df15514c596d30ccc889af14
Rocio Garrido <rocioganon@gmail.com>
2024-12-03 13:01:05 +0100

    refactor: move projects section to projects component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from "../about/about.component";
+import { ProjectsComponent } from "../projects/projects.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutComponent],
+  imports: [AboutComponent, ProjectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
