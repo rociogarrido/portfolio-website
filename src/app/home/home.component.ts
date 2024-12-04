5cf0c0f8a6bbd606d3ac790b94f4276c244f3cdb
Rocio Garrido <rocioganon@gmail.com>
2024-12-04 11:43:54 +0100

    refactor: move menu section to header component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { AboutComponent } from "../about/about.component";
 import { ProjectsComponent } from "../projects/projects.component";
 import { ResumeComponent } from "../resume/resume.component";
 import { ContactComponent } from "../contact/contact.component";
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutComponent, ProjectsComponent, ResumeComponent, ContactComponent],
+  imports: [AboutComponent, ProjectsComponent, ResumeComponent, ContactComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
