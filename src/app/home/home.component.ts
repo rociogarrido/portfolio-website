ba0b587206e465478817c6f4dc1edb3e71c0c298
Rocio Garrido <rocioganon@gmail.com>
2024-12-04 11:25:03 +0100

    refactor: move contact section to contact component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { AboutComponent } from "../about/about.component";
 import { ProjectsComponent } from "../projects/projects.component";
 import { ResumeComponent } from "../resume/resume.component";
+import { ContactComponent } from "../contact/contact.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutComponent, ProjectsComponent, ResumeComponent],
+  imports: [AboutComponent, ProjectsComponent, ResumeComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
