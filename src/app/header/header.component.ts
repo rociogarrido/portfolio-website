4fdc86e20110197e4553e2f8c898a4d494dbbbcc
Rocio Garrido <rocioganon@gmail.com>
2024-12-05 11:54:47 +0100

    styles: implement dark mode

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,8 +5,13 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  isDarkMode: boolean = false;
 
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-mode', this.isDarkMode);
+  }
 }
