4c8e022175219831b2a4405059ddcf2f87b27b9c
Rocio Garrido <rocioganon@gmail.com>
2024-12-06 10:19:29 +0100

    fix: fix issue with dark mode in icons

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class ContactComponent {
 
