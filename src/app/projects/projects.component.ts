269cf630b682fc0c49e02f8e60ac87ff13179d56
Rocio Garrido <rocioganon@gmail.com>
2024-12-11 12:29:59 +0100

    feat: add second and third project

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,21 +12,23 @@ export class ProjectsComponent {
   projects = [
     {
       title: 'Hotel App',
-      description: 'CRUD application to manage hotel reservations',
+      description:
+        'CRUD application to manage hotel reservations built with Angular',
       image: 'assets/images/hotel-app.jpg',
       link: 'https://rociogarrido.github.io/hotel-app/',
     },
     {
-      title: 'Project 2',
-      description: 'This is the description of project 2.',
-      image: 'assets/images/project2.jpg',
-      link: 'https://example.com/project2',
+      title: 'Shopping cart',
+      description:
+        'E-commerce application made with React, TypeScript and Material-UI',
+      image: 'assets/images/shopping-cart.jpg',
+      link: 'https://rociogarrido.github.io/shopping-cart/',
     },
     {
-      title: 'Project 3',
-      description: 'This is the description of project 3.',
-      image: 'assets/images/project3.jpg',
-      link: 'https://example.com/project3',
+      title: 'Music player',
+      description: 'Music player built with pure HTML, CSS and JS.',
+      image: 'assets/images/music-player.jpg',
+      link: 'https://rociogarrido.github.io/music-player/',
     },
   ];
 }
